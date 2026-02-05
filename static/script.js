3c35c0c054a1ae57965d39ab6f35297220e24548
vinitjangir08 <vinit.jangir2025@vitstudent.ac.in>
2026-02-05 23:41:22 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,23 +1,24 @@
 async function loadStatus(){
-let res = await fetch("/status");
-let data = await res.json();
+  const res = await fetch("/status");
+  const data = await res.json();
 
-let box = document.getElementById("box");
-let info = document.getElementById("info");
+  const box = document.getElementById("box");
+  const info = document.getElementById("info");
 
-box.innerHTML = data.desk;
+  box.innerHTML = data.desk;
 
-if(data.desk=="Occupied"){
-box.className="status-box occupied";
-}else{
-box.className="status-box empty";
+  if(data.desk === "Occupied"){
+    box.className = "status-box occupied";
+  } else {
+    box.className = "status-box empty";
+  }
+
+  info.innerHTML = `
+    <b>Device:</b> Arduino Uno<br>
+    <b>Sensor:</b> HC-SR04 Ultrasonic<br>
+    <b>Distance:</b> ${data.distance}<br>
+    <b>Status Update:</b> Just now
+  `;
 }
 
-info.innerHTML = `
-Device: ${data.device}<br>
-Distance: ${data.distance}<br>
-Location: ${data.location}
-`;
-}
-
-setInterval(loadStatus,3000);
+setInterval(loadStatus, 3000);
